931a3e3552965c193d889fd7b6681b7022e8b3bc
Matt <fernandes@alumni.harvard.edu>
2020-09-11 13:57:07 +0000

    finished execise 1

--- pair_programming/PP2/exercise_1.sh
+++ pair_programming/PP2/exercise_1.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-git add $1
-git commit -m $2
+read -r -p "File name: " fn
+
+git add $fn
+git status
+
+read -r -p "Do you wish to continue: (Y or N) " answer
+
+if [ "$answer" = "N" ] || [ "$answer" = "n" ]; then
+	exit
+fi
+
+read -r -p "Commit Message: " msg
+
+git commit -m $msg
+
+git status
+
+read -r -p "Do you wish to continue: (Y or N) " answer
+
+if [ "$answer" = "N" ] || [ "$answer" = "n" ]; then
+	exit
+fi
+
 git push
+
