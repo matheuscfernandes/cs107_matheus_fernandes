7793970da8b2df16c2d2773c357ea85dce98b92d
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-09-14 17:12:02 -0500

    fixed error message in homework

--- homeworks/HW1/P3.sh
+++ homeworks/HW1/P3.sh
@@ -2,5 +2,5 @@
 grep -c "[0-9]" apollo13.txt > apollo_out.txt
 grep --help | grep "^  -c"
 ls *.py | wc -l
-find . -type f ! -perm -o+w,o+r | wc -l
-find . -maxdepth 1 ! -perm -o+w,o+r | wc -l
+find . -type f ! -perm -o+w,o+r 2>/dev/null | wc -l
+find . -maxdepth 1 ! -perm -o+w,o+r 2>/dev/null | wc -l
