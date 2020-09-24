81631b4b12636907f174b45af90571b353069780
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-09-24 16:31:01 -0500

    completed P3a

--- homeworks/HW2/P3a.py
+++ homeworks/HW2/P3a.py
@@ -1,9 +1,17 @@
+#closure function initiating the balance in the bank
 def bank(balance):
+    #inner function that decreases the nonlocal variable by the amount of the withdrawal
     def withdraw(amount):
+        nonlocal balance
         balance-=amount
+        #print the latest balance
         print(balance) 
+    #return the withdraw function
     return withdraw
 
+#initiate the bank account with a certain amount
 bal=bank(100)
-bal(30)
+#withdraw 50 from the balance
+bal(50)
+#withdraw 30 from the new current balance
 bal(30)
