41cdf9cd85777f1f53c6dcea5d39563b78df5d79
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-09-25 22:53:48 -0500

    p3 changes to make it work

--- homeworks/HW2/P3a.py
+++ homeworks/HW2/P3a.py
@@ -1,17 +1,27 @@
 #closure function initiating the balance in the bank
 def bank(balance):
-    #inner function that decreases the nonlocal variable by the amount of the withdrawal
+    #inner function that decreases the balance variable by the amount of the withdrawal
     def withdraw(amount):
-        nonlocal balance
-        balance-=amount
+        try:
+            new_bal=new_bal-amount
+            print('updating balance')
+        except:
+            new_bal=balance-amount
         #print the latest balance
-        print(balance) 
+        print(new_bal) 
     #return the withdraw function
     return withdraw
 
 #initiate the bank account with a certain amount
-bal=bank(100)
+wd=bank(100)
+
 #withdraw 50 from the balance
-bal(50)
-#withdraw 30 from the new current balance
-bal(30)
+wd(50)
+wd(30)
+
+#checking for basic exceptions
+wd(150)
+wd(-50)
+
+#printing discussion on what is going on in this problem
+print('This does not work because there is no way to keep the information stored in the new_bal variable (as this variable is maintained in the local frame and is reset whenever the function is over) as this variable is not a global variable unlike the balance variable.')
