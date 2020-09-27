a242d511042df2ca00c7c5e1e5eb8576f497a7a2
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-09-27 11:54:03 -0500

    created p4 file with skelleton provided

---
+++ homeworks/HW2/P4.py
@@ -0,0 +1,20 @@
+import numpy
+import matplotlib.pyplot
+import datetime
+
+### Closure defined up here
+
+currentDT = datetime.datetime.now()
+hour = currentDT.hour
+minute = currentDT.minute
+second = currentDT.second
+
+# Calculate theta in degrees for each hand
+
+# Specify the length of hour, minute and second hands
+
+# hour_hand = name_of_closure(length_of_hour_hand)
+
+# x_hour, y_hour = hour_hand(theta_hour)
+
+# Plot the clock
