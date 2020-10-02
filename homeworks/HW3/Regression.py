e64f69ce422502754cc3d95ac5d720c404d8a21f
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-10-01 23:33:21 -0500

    initialized the skelleton of regression.py - P2.1

---
+++ homeworks/HW3/Regression.py
@@ -0,0 +1,19 @@
+class Regression():
+
+    def __init__(self):
+        # your code
+
+    def get_params(self):
+        # your code
+
+    def set_params(self, **kwargs):
+        # your code
+
+    def fit(self, X, y):
+        # your code
+
+    def predict(self, X):
+        # your code
+
+    def score(self, X, y):
+        # your code
