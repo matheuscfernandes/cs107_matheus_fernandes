26af2cbc22fc4cd8ccde1700d0c1450241efaa9c
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-10-04 22:54:01 -0500

    cleanded model evaluations for p2

--- homeworks/HW3/model_performance.py
+++ homeworks/HW3/model_performance.py
@@ -11,29 +11,37 @@ X_train, X_test, y_train, y_test = train_test_split(dataset['data'],
                                                     random_state=42)
 
 
+#create empty lists to store the score
 scoreRR=[]
 scoreLR=[]
 
+#initialize and compote the score for LR model since it does not depend on alpha
 LRModel=reg.LinearRegression()
 LRModel.set_params()
 LRModel.fit(X_train, y_train);
 score=LRModel.score(X_test,y_test)
 
+#initialize instance of RR
 RRModel=reg.RidgeRegression()
 
+#loop over different alphas in log10 fashion
 for alpha in 10**np.linspace(-2,1,15):
     RRModel.set_params(alpha=alpha)
     RRModel.fit(X_train, y_train);
     scoreRR.append([alpha,RRModel.score(X_test,y_test)])
     scoreLR.append([alpha,score])
 
+#convert the score lists into numpy arrauys
 scoreRR=np.array(scoreRR)
 scoreLR=np.array(scoreLR)
 
+#plot data in semilogz form
 plt.semilogx(scoreRR[:,0],scoreRR[:,1],'o-',label='Ridge Regression')
 plt.semilogx(scoreLR[:,0],scoreLR[:,1],'o-',label='Linear Regression')
 plt.xlabel(r'$\alpha$')
 plt.ylabel(r'$R^2$ score')
 plt.legend()
+
+#save and show plot
 plt.savefig('P2F.png',format='png',dpi=300)
 plt.show()
--- homeworks/HW3/model_scoring.py
+++ homeworks/HW3/model_scoring.py
@@ -8,18 +8,26 @@ X_train, X_test, y_train, y_test = train_test_split(dataset['data'],
                                                     test_size=0.2, 
                                                     random_state=42)
 
+
 alpha = 0.1
+#initialize LR model
 LRModel=reg.LinearRegression()
 LRModel.set_params()
 
+#initilizse the RR model
 RRModel=reg.RidgeRegression()
 RRModel.set_params(alpha=alpha)
 
+#put both models into a list 
 models = [LRModel,RRModel]
 
-err=[]
+#initialize empty list to store the scores of the models
+score=[]
+
+#iterate over the models
 for model in models:
     model.fit(X_train, y_train);
-    print(model.params['beta'])
-    err.append(model.score(X_test,y_test))
-print(err)
+    score.append(model.score(X_test,y_test))
+
+#print the computed scores for the different models in nice format
+print('LRModel R2 Score: {}\nRRModel R2 Score: {}'.format(score[0],score[1]))
