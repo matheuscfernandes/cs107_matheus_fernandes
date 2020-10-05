3d2172c795b01620864c0e514193f4fa1b12541f
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-10-05 13:06:59 -0500

    added radom initializer in comments this is in case that is needed for pp4 although not specified.

--- pair_programming/PP4/exercise_1.py
+++ pair_programming/PP4/exercise_1.py
@@ -31,6 +31,12 @@ w2 = np.array([[2]*shape2[0]]*shape2[1]).T
 b1 = np.array([[2]*shape1[1]])
 b2 = np.array([[1]*shape2[1]])
 
+# Initialize weights and biases with random generator
+# w1 = np.random.uniform(0,1,shape1[0]*shape1[1]).reshape(shape1)
+# w2 = np.random.uniform(0,1,shape2[0]*shape2[1]).reshape(shape2)
+# b1 = np.random.uniform(0,1,shape1[1])
+# b2 = np.random.uniform(0,1,shape2[1])
+
 # Run through the network
 h1 = layer1(t, w1, b1) # First layer
 h2 = layer2(h1, w2, b2) # Last layer
