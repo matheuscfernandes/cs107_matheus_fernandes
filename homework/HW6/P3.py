b56f47f81546ad077cb3976fa41480e532e03d33
Matheus Fernandes <matheuscfernandes@gmail.com>
2020-11-20 22:01:36 -0500

    changes to p3

--- homework/HW6/P3.py
+++ homework/HW6/P3.py
@@ -59,6 +59,10 @@ def timeit(ns=(10, 20, 50, 100, 200, 500), pqclass=PriorityQueue, n_average=5):
     return elapsed
 
 
+class NaivePriorityQueue(PriorityQueue):
+
+
+
 if __name__ == "__main__":
     q = NaivePriorityQueue(2)
     q.put(1)
